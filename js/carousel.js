2d46a081157a57ad9b4113cf7becb8e4053c333b
Morad17 <morad123@hotmail.co.uk>
2021-01-27 13:47:28 +0000

    carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,6 @@
 const track = document.querySelector('.carousel_track');
 const slides = Array.from(track.children);
-const dotsNavs = document.querySelector('.carousel_nav');
+const dotsNav = document.querySelector('.carousel_nav');
 const dots = Array.from(dotsNav.children);
 
 const slideWidth = slides[0].getBoundingClientRect().width;
@@ -11,8 +11,30 @@ const setSlidePosition = (slide, index) => {
 };
 slides.forEach(setSlidePosition);
 
-const moveToSlide = () => {
+const moveToSlide = (track, currentSlide, targetSlide) => {
     track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
     currentSlide.classList.remove('active-slide');
     nextSlide.classList.add('active-slide')
 }
+
+//Update bottom Dots
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+}
+
+dotsNav.addEventListener('click', e => {
+    //Which indicator clicked on
+    const targetDot = e.target.closest('button');
+
+    if (!targetDot) return;
+    
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot)
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    
+})
