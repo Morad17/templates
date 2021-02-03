c1ec332371d5314d59c8ff1294fe0cfdeb1b4f71
Morad17 <morad123@hotmail.co.uk>
2021-02-03 12:01:04 +0000

    finished homepage and carousel fully functioning

--- js/carousel.js
+++ js/carousel.js
@@ -1,47 +1,51 @@
-const slideContainer = document.querySelector('.carousel');
-const slide = document.querySelector('.slides')
-const nextBtn = document.getElementById('next-btn');
-const prevBtn = document.getElementById('prev-btn');
-const interval = 1000;
+var slides = document.querySelectorAll('.slide');
+var btns = document.querySelectorAll('.btn');
+let currentSlide = 1;
 
-let slides = document.querySelectorAll('.slide')
-let index = 1;
-// // Arrange Slides//
+//Manual Slide Navigation
+var manualNav = function(manual){
+    slides.forEach((slide) =>{
+        slide.classList.remove('active');
 
-const firstClone = slides[0].cloneNode(true);
-const lastClone = slides[slides.length - 1].cloneNode(true)
+        btns.forEach((btn) => {
+           btn.classList.remove('active');
+        });
+    });
+    
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+}
 
-firstClone.id = 'first-clone';
-lastClone.id = 'last-clone';
-
-slide.append(firstClone);
-slide.prepend(lastClone)
-
-const slideWidth = slides[index].clientWidth;
-
-slide.style.transform = `translateX(${-slideWidth * index}px)`;
-
-console.log(slides)
-const startSlide = () => {
-    setInterval(() =>{
-    index++;
-    slide.style.transform = `translateX(${-slideWidth * index}px)`;
-    slide.style.transition = '.7s';
-    }, interval);
-};
-startSlide();
-slide.addEventListener('transitionend', () => {
-    firstClone.id = 'first-clone';
-    lastClone.id = 'last-clone';
-    slides = document.querySelectorAll('.slide');
-    if(slides[index].id === firstClone.id){
-        slides.style.transition = 'none';
-        index = 1;
-        slide.style.transform = `translateX(${-slideWidth * index}px)`;
-    }
+btns.forEach((btn, i) =>{
+    btn.addEventListener("click", () =>{
+        manualNav(i);
+        currentSlide = i;
+    });
 });
 
+//Carousel autoplay navigation 
+var repeat = function(activeClass){
+    let active = document.getElementsByClassName('active');
+    let i  = 1;
 
+    var repeater = () => {
+        setTimeout(function(){
+            [...active].forEach((activeSlide) => {
+                activeSlide.classList.remove('active');
+                });
 
-
-
+                slides[i].classList.add('active');
+                btns[i].classList.add('active');
+                i++;
+                if( slides.length == i){
+                    i = 0;
+                }
+                if(i >= slides.length){
+                    return;
+                }
+                repeater();
+            }, 6000)
+        }
+        repeater();
+    }
+    repeat();
